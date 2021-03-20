5dd9f357fc0e9def08ba6e3e102c22b5b7ced021
meyjac23 <jmeyer32@wisc.edu>
2021-03-20 11:28:14 -0500

    Add DatabaseManager.java stubs

---
+++ app/src/main/java/com/example/wiscpets/DatabaseManager.java
@@ -0,0 +1,91 @@
+package com.example.wiscpets;
+
+import org.json.JSONObject;
+
+public class DatabaseManager {
+
+    public JSONObject validate() {
+        String operation = "get";
+        return null;
+    }
+
+    public JSONObject getSicknesses() {
+        String operation = "get";
+        return null;
+    }
+
+    public JSONObject getPrescriptions() {
+        String operation = "get";
+        return null;
+    }
+
+    public JSONObject getAppointments() {
+        String operation = "get";
+        return null;
+    }
+
+    public JSONObject getPetTable() {
+        String operation = "get";
+        return null;
+    }
+
+    public JSONObject getPetData() {
+        String operation = "get";
+        return null;
+    }
+
+    public boolean modifyAccount() {
+        String operation = "put";
+        return false;
+    }
+
+    public boolean addPrescription() {
+        String operation = "post";
+        return false;
+    }
+
+    public boolean addAppointment() {
+        String operation = "post";
+        return false;
+    }
+
+    public boolean addPet() {
+        String operation = "post";
+        return false;
+    }
+
+    public boolean modifyPrescription() {
+        String operation = "put";
+        return false;
+    }
+
+    public boolean modifyAppointment() {
+        String operation = "put";
+        return false;
+    }
+
+    public boolean modifyPet() {
+        String operation = "put";
+        return false;
+    }
+
+    public boolean deletePet() {
+        String operation = "delete";
+        return false;
+    }
+
+    public boolean deletePrescription() {
+        String operation = "delete";
+        return false;
+    }
+
+    public boolean deleteAccount() {
+        String operation = "delete";
+        return false;
+    }
+
+    public boolean addAccount() {
+        String operation = "post";
+        return false;
+    }
+}
