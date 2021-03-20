93268e024f4c6845e1ccfe38a2a197f575fa3363
meyjac23 <jmeyer32@wisc.edu>
2021-03-20 13:05:15 -0500

    createToken works

--- app/src/main/java/com/example/wiscpets/DatabaseManager.java
+++ app/src/main/java/com/example/wiscpets/DatabaseManager.java
@@ -1,100 +1,103 @@
 package com.example.wiscpets;
 
+import android.util.Base64;
+
 import org.json.JSONObject;
 
 public class DatabaseManager {
 
     public String createToken(String username, String password) {
-        return "";
+        String toEncode = username + ":" + password;
+        return Base64.encodeToString(toEncode.getBytes(), Base64.DEFAULT);
     }
 
     public JSONObject validate(String username, String password) {
-        String operation = "get";
+        String requestType = "GET";
         return null;
     }
 
     public JSONObject getSicknesses(String species, String[] symptoms) {
-        String operation = "get";
+        String requestType = "GET";
         return null;
     }
 
     public JSONObject getPrescriptions(String token, int petId) {
-        String operation = "get";
+        String requestType = "GET";
         return null;
     }
 
     public JSONObject getAppointments(String token) {
-        String operation = "get";
+        String requestType = "GET";
         return null;
     }
 
     public JSONObject getPetTable() {
-        String operation = "get";
+        String requestType = "GET";
         return null;
     }
 
     public JSONObject getPetData(String token, int petId) {
-        String operation = "get";
+        String requestType = "GET";
         return null;
     }
 
     public boolean modifyAccount(String username, String password, long phoneNumber) {
-        String operation = "put";
+        String requestType = "PUT";
         return false;
     }
 
     public boolean addPrescription(String medName, String dosage, String timePeriod) {
-        String operation = "post";
+        String requestType = "POST";
         return false;
     }
 
     public boolean addAppointment(String token, int petId, String vetToken) {
-        String operation = "post";
+        String requestType = "POST";
         return false;
     }
 
     public boolean addPet(String token, String species, String breed, String birthday) {
-        String operation = "post";
+        String requestType = "POST";
         return false;
     }
 
     public boolean modifyPrescription(String token, int petId, String prescription, String dosage) {
-        String operation = "put";
+        String requestType = "PUT";
         return false;
     }
 
     public boolean modifyAppointment(String token, String oldDate, String newDate) {
-        String operation = "put";
+        String requestType = "PUT";
         return false;
     }
 
     public boolean modifyPet(String token, int petId, String name, String breed, String birthday) {
-        String operation = "put";
+        String requestType = "PUT";
         return false;
     }
 
     public boolean deletePet(String token, int petId) {
-        String operation = "delete";
+        String requestType = "DELETE";
         return false;
     }
 
     public boolean deletePrescription(String token, int petId, String prescription) {
-        String operation = "delete";
+        String requestType = "DELETE";
         return false;
     }
 
     public boolean deleteAccount(String token) {
-        String operation = "delete";
+        String requestType = "DELETE";
         return false;
     }
 
     public boolean deleteAppointment(String token, String apptDate) {
-        String operation = "delete";
+        String requestType = "DELETE";
         return false;
     }
 
     public boolean addAccount(String email, String password, String name, long phoneNumber, String address) {
-        String operation = "post";
+        String requestType = "POST";
         return false;
     }
 }
